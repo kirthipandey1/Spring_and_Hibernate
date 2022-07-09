bd7c3dd978545d544ff37929b5bca2fe89766255
kirthipandey1 <siyalishiya@gmail.com>
2022-07-09 08:40:30 +0530

    injecting fields using property file.

--- Spring-demo-NoXML-Annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ Spring-demo-NoXML-Annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -10,11 +10,17 @@ public class SwimJavaConfigDemoApp {
 				AnnotationConfigApplicationContext(SportConfig.class);
 		
 		//name swimCoach is from config file
-		Coach theCoach = context.getBean("swimCoach",Coach.class);
+		//Coach theCoach = context.getBean("swimCoach",Coach.class);
+		
+		SwimCoach theCoach = context.getBean("swimCoach",SwimCoach.class);
 		
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
+		
+		//calling the property value 
+		
+		System.out.println(theCoach.getEmail()+"     "+theCoach.getTeam());
 		
 		context.close();
 		
