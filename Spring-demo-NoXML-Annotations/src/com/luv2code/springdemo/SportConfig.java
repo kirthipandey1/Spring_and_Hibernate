185e02e275af3d9eb59bb44b211fde557b19ae66
kirthipandey1 <siyalishiya@gmail.com>
2022-07-10 08:48:27 +0530

    just to remove warning commented the unused import

--- Spring-demo-NoXML-Annotations/src/com/luv2code/springdemo/SportConfig.java
+++ Spring-demo-NoXML-Annotations/src/com/luv2code/springdemo/SportConfig.java
@@ -1,7 +1,7 @@
 package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
